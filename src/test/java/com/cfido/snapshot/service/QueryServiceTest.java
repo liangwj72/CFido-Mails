e505c7413b2edc2557579610e333c1b2dc23d94f
梁韦江 <liangwj72@qq.com>
2016-03-25 17:38:26 +0800

    改变文件位置，为了解决热部署的问题

--- src/test/java/com/cfido/snapshot/service/QueryServiceTest.java
+++ src/test/java/com/cfido/snapshot/service/QueryServiceTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.cfido.snapshot.service;
 
 import java.util.List;
@@ -23,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.cfido.snapshot.MainApplication;
@@ -31,12 +17,13 @@ import com.cfido.snapshot.domain.Mail;
 import com.cfido.snapshot.repository.MailRepository;
 
 /**
- * Integration tests for {@link CityRepository}.
+ * Integration tests
  *
  * @author Oliver Gierke
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(MainApplication.class)
+@ActiveProfiles("dev")
 public class QueryServiceTest {
 
 	@Autowired
